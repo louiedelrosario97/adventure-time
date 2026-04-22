96956bfc1bebf052ebb6f0f1d0b8edf80d813e57
Louie D <louie.delrosario97@gmail.com>
2026-04-22 11:16:49 -0700

    try and catch built for reading adventure1.csv

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,6 +1,34 @@
-package com.pluralsight;public class AdventureTime {
-    public static void main(String[] args) {
-        
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class AdventureTime
+{
+    public static void main(String[] args)
+    {
+        loadAdventure();
+    }
+    public static void loadAdventure()
+    {
+        try
+        {
+            FileReader fileReader = new FileReader("adventure1.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while(line != null)
+            {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+        }
+        catch (Exception c)
+        {
+
+        }
     }
 }
 
