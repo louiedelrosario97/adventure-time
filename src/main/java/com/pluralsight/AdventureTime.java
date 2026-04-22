8855242469c46afe53973a41e91cda29795e2785
Louie D <louie.delrosario97@gmail.com>
2026-04-22 14:56:19 -0700

    end of lecture commit

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -2,30 +2,49 @@ package com.pluralsight;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AdventureTime
 {
-    public static void main(String[] args)
+    static void main();
     {
         loadAdventure();
     }
     public static void loadAdventure()
     {
+        ArrayList<StepClass> adventureSteps = new ArrayList<>(); // Array lists can change index amount as needed
+
         try
         {
-            FileReader fileReader = new FileReader("adventure1.csv"); // import FileReader to reach file
+            FileReader fileReader = new FileReader("adventure1.csv"); // Creat FileReader class to reach file
             BufferedReader bufferedReader = new BufferedReader(fileReader); // Formats the FileReader to read efficiently
 
-            String line = bufferedReader.readLine(); // Skips header
+            String line = bufferedReader.readLine(); // Skips the first line in the file from being read (header)
 
-            while(line != null) // Loops to each line of the file until next line is null
+            line = bufferedReader.readLine();
+
+            while(line != null) // Loops to each line of the file until next line's value is null
             {
-                System.out.println(line); // Prints line
+                System.out.println(line); // Prints variable 'line' which we assigned to hold and read the text of 'bufferedReader' on line 20
                 line = bufferedReader.readLine(); // Goes to next line in file to print
-            }
 
+                String[]columns = line.split("\\|");
+                int id = Integer.parseInt(columns[0]);
+                String storyText = columns[1];
+                String option1Text = columns [2];
+                int option1NextId = Integer.parseInt(columns [3]);
+                String option2Text = columns [4];
+                int option2NextId = Integer.parseInt(columns[5]);
+
+                // create a Step from the data in the current line
+                StepClass stepClass = new StepClass(id, storyText, option1Text, option1NextId, option2Text, option2NextId );
+
+                adventureSteps.add(stepClass); // Put 'stepClass' into an array list
+            }
+            bufferedReader.close();
         }
-        catch (Exception c)
+        catch (Exception e)
         {
 
         }
---
+++ src/main/java/com/pluralsight/StepClass.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+public class StepClass
+{       // Data Members
+    private int id;
+    private String storyText;
+    private String option1Text;
+    private int option1NextId;
+    private String option2Text;
+    private int option2NextId;
+
+    public StepClass(int id, String storyText, String option1Text, int option1NextId, String option2Text, int option2NextId)
+    {   // Constructors
+        this.id = id;
+        this.storyText = storyText;
+        this.option1Text = option1Text;
+        this.option1NextId = option1NextId;
+        this.option2Text = option2Text;
+        this.option2NextId = option2NextId;
+    }
+         // Getters
+    public int getId() { return id; }
+    public String getStoryText() { return storyText; }
+    public String getOption1Text() { return option1Text; }
+    public int getOption1NextId() { return option1NextId; }
+    public String getOption2Text() { return option2Text; }
+    public int getOption2NextId() { return option2NextId; }
+
+        // Setters
+    public void setId(int id) { this.id = id;}
+    public void setStoryText(String storyText) { this.storyText = storyText; }
+    public void setOption1Text(String option1Text) { this.option1Text = option1Text; }
+    public void setOption1NextId(int option1NextId) { this.option1NextId = option1NextId; }
+    public void setOption2Text(String option2Text) { this.option2Text = option2Text; }
+    public void setOption2NextId(int option2NextId) { this.option2NextId = option2NextId; }
+
+}
+
+
