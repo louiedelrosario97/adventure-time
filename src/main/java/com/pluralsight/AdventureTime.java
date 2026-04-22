6a9213089a2ff1cb5f297e26b546c770af56830f
Louie D <louie.delrosario97@gmail.com>
2026-04-22 11:27:16 -0700

    try and catch built for reading adventure1.csv

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -13,15 +13,15 @@ public class AdventureTime
     {
         try
         {
-            FileReader fileReader = new FileReader("adventure1.csv");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            FileReader fileReader = new FileReader("adventure1.csv"); // import FileReader to reach file
+            BufferedReader bufferedReader = new BufferedReader(fileReader); // Formats the FileReader to read efficiently
 
-            String line = bufferedReader.readLine();
+            String line = bufferedReader.readLine(); // Skips header
 
-            while(line != null)
+            while(line != null) // Loops to each line of the file until next line is null
             {
-                System.out.println(line);
-                line = bufferedReader.readLine();
+                System.out.println(line); // Prints line
+                line = bufferedReader.readLine(); // Goes to next line in file to print
             }
 
         }
